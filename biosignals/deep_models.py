aa47c4a8c49910c27c9b1ce0113c4984a0231468
Kevin Jin <jinkevin321@gmail.com>
2022-11-28 17:33:45 -0800

    implement model interface for deep learning models

--- biosignals/deep_models.py
+++ biosignals/deep_models.py
@@ -1,6 +1,16 @@
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import LSTM, GRU, Activation  # Dropout, BatchNormalization
+from typing import Any, Dict, List, Optional, Tuple
+from biosignals.models import Strategy, Model, Results, split_df, sk_load_single, sk_load_multi
+from numpy.random import RandomState
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+import numpy as np
+import biosignals.prepare as bp
+import biosignals.evaluation as be
+import tensorflow as tf
+
 # from keras.layers.embeddings import Embedding
 # from keras.preprocessing import sequence
 
@@ -11,8 +21,84 @@ from keras.layers import LSTM, GRU, Activation  # Dropout, BatchNormalization
 # They used keras so I've just kept it that way for now... We can switch to torch later if we want...
 
 
+class SequentialModel(Model):
+
+    def __init__(self, model_class: Any, model_args: Dict[str, Any], strategy: Strategy):
+        if strategy == Strategy.ENSEMBLE:
+            raise Exception('ensemble not supported in this implementation')
+        self._usePCA = model_args.get("usePCA", None)
+        self._model = Sequential()
+        self._scaler = StandardScaler()
+        self._model_class = model_class
+        self._pca = PCA(n_components=64)
+        self._strategy = strategy
+
+    def _load_one(self, ld: bp.FrameLoader, rand: Optional[RandomState]) -> Tuple[np.ndarray, np.ndarray]:
+        if self._strategy == Strategy.COMBINED:
+            return split_df(*sk_load_single(ld), rand=rand)
+        else:
+            assert self._strategy == Strategy.MULTI
+            return split_df(*sk_load_multi(ld, bp.NUM_CLUSTERS), rand=rand)
+
+    def _load_all(self, lds: List[bp.FrameLoader],
+                  isTraining: bool,
+                  rand: Optional[RandomState]) -> Tuple[np.ndarray, np.ndarray]:
+        xs = []
+        ys = []
+        for ld in lds:
+            x, y = self._load_one(ld, rand)
+            xs.append(x)
+            ys.append(y)
+        x = np.concatenate(xs)
+        y = np.concatenate(ys)
+        x = tf.convert_to_tensor(x, dtype=tf.float64)
+        self._scaler.fit(x)
+        x = self._scaler.transform(x)
+        if self._usePCA:
+            if isTraining:
+                self._pca.fit(x)
+            x = self._pca.transform(x)
+        x = tf.expand_dims(x, axis=1)
+        y = tf.convert_to_tensor(y, dtype=tf.int32)
+        return (x, y)
+
+    def _test_one(self, x: np.ndarray, y_true: np.ndarray) -> Results:
+        y_pred = self._model.predict(x)
+        be.evaluate_model(y_pred, y_true)
+        y_pred = tf.squeeze(y_pred, axis=1)
+        return Results.from_pred(y_true, y_pred)
+
+    # Train on all train/validate datasets
+    # Return final results for training set
+    def train_all(
+        self,
+        train_loaders: List[bp.FrameLoader],
+        validate_loaders: List[bp.FrameLoader],
+        rand: Optional[RandomState],
+        num_epochs=30,
+        batch_size=64,
+        verbose=1,
+    ) -> Results:
+        train_data, train_labels = self._load_all(train_loaders, True, rand)
+        self._model = Sequential()
+        self._model.add(self._model_class(512, input_shape=(train_data.shape[1], train_data.shape[2]),
+                        return_sequences=True))
+        self._model.add(Activation("relu"))
+        self._model.add(self._model_class(256))
+        self._model.add(Dense(1, activation='sigmoid'))
+        self._model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+        print(self._model.summary())
+        self._model.fit(train_data, train_labels, epochs=num_epochs, batch_size=batch_size, verbose=verbose)
+        return self._test_one(train_data, train_labels)
+
+    # Test on all test datasets
+    def test_all(self, test_loaders: List[bp.FrameLoader]) -> Results:
+        x, y = self._load_all(test_loaders, False, None)
+        return self._test_one(x, y)
+
+
 # Constructs and returns a GRU model. Call predict(<data>) on the returned model to make predictions on <data>.
-def GRU_Model(train_data, train_labels, num_epochs = 10, batch_size = 64, verbose = 1):
+def GRU_Model(train_data, train_labels, num_epochs=10, batch_size=64, verbose=1):
     model = Sequential()
     model.add(GRU(512, input_shape=(train_data.shape[1], train_data.shape[2]), return_sequences=True))
     # model.add(BatchNormalization())
@@ -28,7 +114,7 @@ def GRU_Model(train_data, train_labels, num_epochs = 10, batch_size = 64, verbos
 
 
 # Constructs and returns a LSTM model. Call predict(<data>) on the returned model to make predictions on <data>.
-def LSTM_Model(train_data, train_labels,  num_epochs = 10, batch_size = 64, verbose = 1):
+def LSTM_Model(train_data, train_labels, num_epochs=10, batch_size=64, verbose=1):
     model = Sequential()
     model.add(LSTM(512, input_shape=(train_data.shape[1], train_data.shape[2]), return_sequences=True))
     # model.add(BatchNormalization())
@@ -41,3 +127,20 @@ def LSTM_Model(train_data, train_labels,  num_epochs = 10, batch_size = 64, verb
     print(model.summary())
     model.fit(train_data, train_labels, epochs=num_epochs, batch_size=batch_size, verbose=verbose)
     return model
+
+
+# Test training with some sklearn models
+def test_models():
+    rand = RandomState(42)
+    skmodels = [
+        (LSTM, {"usePCA": False}, Strategy.MULTI),
+        (LSTM, {"usePCA": True}, Strategy.MULTI),
+        (GRU, {"usePCA": False}, Strategy.MULTI),
+        (GRU, {"usePCA": True}, Strategy.MULTI),
+    ]
+    for klass, args, strat in skmodels:
+        print(f'Training model {klass} {args} {strat}')
+        model = SequentialModel(klass, args, strat)
+        _, tres = model.execute('rand', rand)
+        print(tres)
+        print('accuracy', tres.accuracy)
